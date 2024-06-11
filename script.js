d005235208f5cca1ecb549dcb17fdde4b7b29d58
GeyserHD <gguerrer9@cuc.edu.co>
2024-06-10 21:14:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -29,8 +29,8 @@ function updateHtmlOutput() {
   <!DOCTYPE html>
 <html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portada del Juego</title>
     <style>
         body {
